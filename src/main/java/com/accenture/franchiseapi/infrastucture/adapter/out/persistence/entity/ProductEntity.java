55487c618177df92965559af0196089d5dca62a5
Santiago Montoya <santiagomontoya997@gmail.com>
2026-08-06 09:56:40 -0500

    feat: add ProductEntity class for persistence layer

---
+++ src/main/java/com/accenture/franchiseapi/infrastucture/adapter/out/persistence/entity/ProductEntity.java
@@ -0,0 +1,69 @@
+package com.accenture.franchiseapi.infrastucture.adapter.out.persistence.entity;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("products")
+public class ProductEntity implements Persistable<UUID> {
+    @Id
+    private final UUID id;
+    @Column("branch_id")
+    private final UUID branchId;
+    private String name;
+    private int stock;
+    @Transient
+    private final boolean isNewEntity;
+
+    @PersistenceCreator
+    public ProductEntity(UUID id, UUID branchId, String name, int stock) {
+        this.id = id;
+        this.branchId = branchId;
+        this.name = name;
+        this.stock = stock;
+        this.isNewEntity = false;
+    }
+
+    private ProductEntity(UUID id, UUID branchId, String name, int stock, boolean isNewEntity) {
+        this.id = id;
+        this.branchId = branchId;
+        this.name = name;
+        this.stock = stock;
+        this.isNewEntity = isNewEntity;
+    }
+
+    public ProductEntity createNew(UUID id, UUID branchId, String name, int stock) {
+        return new ProductEntity(id, branchId, name, stock, true);
+    }
+
+    public ProductEntity createExisting(UUID id, UUID branchId, String name, int stock) {
+        return new ProductEntity(id, branchId, name, stock, false);
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNewEntity;
+    }
+
+    public UUID getBranchId() {
+        return branchId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
