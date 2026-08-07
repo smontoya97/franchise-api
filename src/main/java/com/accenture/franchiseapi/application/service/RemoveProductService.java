188547285c62b4d11628700fe6e5085e4bfcb618
Santiago Montoya <santiagomontoya997@gmail.com>
2026-08-07 18:59:37 -0500

    feat: implement RemoveProduct use case and add endpoint to ProductController

---
+++ src/main/java/com/accenture/franchiseapi/application/service/RemoveProductService.java
@@ -0,0 +1,24 @@
+package com.accenture.franchiseapi.application.service;
+
+import com.accenture.franchiseapi.application.command.product.RemoveProductCommand;
+import com.accenture.franchiseapi.application.port.in.product.RemoveProductUseCase;
+import com.accenture.franchiseapi.application.port.out.ProductRepositoryPort;
+import com.accenture.franchiseapi.domain.exception.ProductNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class RemoveProductService implements RemoveProductUseCase {
+
+    private final ProductRepositoryPort productRepositoryPort;
+
+    @Override
+    public Mono<Void> execute(RemoveProductCommand command) {
+        return productRepositoryPort.existsByIdAndBranchId(command.productId(), command.branchId())
+                .flatMap(exists -> exists
+                    ? productRepositoryPort.deleteById(command.productId())
+                    : Mono.error(new ProductNotFoundException(command.productId())));
+    }
+}
