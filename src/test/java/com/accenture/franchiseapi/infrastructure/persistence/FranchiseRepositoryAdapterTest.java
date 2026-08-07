c7cad433c5f4d0c941e706b5106ae3306b5b26e3
Santiago Montoya <santiagomontoya997@gmail.com>
2026-08-07 13:09:45 -0500

    feat: add integration tests for AddBranch use case and refactor persistence tests

---
+++ src/test/java/com/accenture/franchiseapi/infrastructure/persistence/FranchiseRepositoryAdapterTest.java
@@ -0,0 +1,49 @@
+package com.accenture.franchiseapi.infrastructure.persistence;
+
+import com.accenture.franchiseapi.domain.model.Branch;
+import com.accenture.franchiseapi.domain.model.Franchise;
+import com.accenture.franchiseapi.domain.model.Product;
+import com.accenture.franchiseapi.domain.model.valueobject.FranchiseId;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FranchiseRepositoryAdapterTest extends AbstractPersistenceTest {
+
+    @Test
+    void shouldSaveAndRetrieveFranchiseWithBranchesAndProducts() {
+        String franchiseName = "Franquicia Medellín";
+        String branchName = "Sucursal Poblado";
+        String productName = "Coca-Cola";
+        int productStock = 10;
+        Franchise franchise = Franchise.create(franchiseName);
+        Franchise saved = franchiseRepositoryAdapter.save(franchise).block();
+        assert saved != null;
+        FranchiseId franchiseId = saved.getId();
+        Branch branch = Branch.create(branchName);
+        Branch savedBranch = branchRepositoryAdapter.save(branch, franchiseId).block();
+        Product product = Product.create(productName, productStock);
+        assert savedBranch != null;
+        productRepositoryAdapter.save(product, savedBranch.getId()).block();
+        int expectedSize = 1;
+
+        StepVerifier.create(franchiseRepositoryAdapter.findById(franchiseId))
+                .assertNext(result -> {
+                    assertEquals(franchiseName, result.getName());
+                    assertEquals(expectedSize, result.getBranches().size());
+                    Branch resultBranch = result.getBranches().getFirst();
+                    assertEquals(branchName, resultBranch.getName());
+                    assertEquals(expectedSize, resultBranch.getProducts().size());
+                    assertEquals(productName, resultBranch.getProducts().getFirst().getName());
+                    assertEquals(productStock, resultBranch.getProducts().getFirst().getStock());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnEmptyMonoWhenFranchiseDoesNotExist() {
+        StepVerifier.create(franchiseRepositoryAdapter.findById(FranchiseId.newId()))
+                .verifyComplete();
+    }
+}
